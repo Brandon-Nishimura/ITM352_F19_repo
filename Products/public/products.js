ef40dff9df72d74fe3d7c19967f9c68e874b25d3
Brandon-Nishimura <54702926+Brandon-Nishimura@users.noreply.github.com>
2019-11-15 23:30:48 -1000

    Add files via upload

---
+++ Products/public/products.js
@@ -0,0 +1,32 @@
+products =
+[
+  {  
+    "model":"Squid (Limited!)",  
+    "price": 7.00,  
+    "image": "/images/product1"
+  },
+  {
+    "model":"Triceratops",  
+    "price": 5.00,  
+    "image": "/images/product2"
+  },
+  {  
+    "model":"Octopus",  
+    "price": 5.00,  
+    "image": "/images/product3"
+  },
+  {  
+    "model":"Slime",  
+    "price": 5.00,  
+    "image": "/images/product4"
+  },
+  {  
+    "model":"Squid",  
+    "price": 5.00,  
+    "image": "/images/product5"
+  }
+]
+
+if(typeof module != 'undefined') {
+  module.exports.products = products;
+}
